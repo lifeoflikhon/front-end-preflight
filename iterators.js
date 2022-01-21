c228cd980b2a9bd5bac823d5dded70a6223b4f8c
lifeoflikhon <mehedi.hasansjs@gmail.com>
2022-01-21 19:39:15 +0600

    feat: added iterator

--- iterators.js
+++ iterators.js
@@ -1,24 +1,45 @@
-for(const number = 0; number < 10; number++) {
-  console.log(number);
+// for(const number = 0; number < 10; number++) {
+//   //console.log(number);
+// }
+
+// const number = 0;
+// while (number < 10) {
+//   //console.log(number);
+//   number++;
+// }
+
+// const number = 0;
+// do {
+//   //console.log(number);
+//   number++;
+// } while (number < 10);
+
+
+// ==================
+const name = '-shi-fat-';
+const letters = [];
+for(let i = 0; i < name.length; i++) {
+  letters.push(name[i]);
 }
-
-const number = 0;
-while (number < 10) {
-  console.log(number);
-  number++;
-}
-
-const number = 0;
-do {
-  console.log(number);
-  number++;
-} while (number < 10);
-
+console.log(letters);
 
 // forEach
+// names.forEach((name) => console.log(name));
+
 // Map
+// console.log(names.map((name) => 'md. ' + name));
+
 // Filter
+// console.log(names.filter((name) => name === 'mehedi'));
+
 // Find
+// console.log(names.find((name) => name === 'mehedi'));
+
 // Reduce
+// console.log(numbers.reduce((total, curr) => total + curr, 0));
+
 // Splice
+// console.log(numbers.splice(1, 2));
+
 // Split
+// console.log(name.split(''));
